f8c1895afa07a5845c19dd30687e27213e626ccd
Rob Sturcke <rob@rslabs.dev>
2024-01-26 22:13:39 -0500

    Initial Commit

---
+++ FloatingTabs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FloatingTabs
+//
+//  Created by Rob Sturcke on 1/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FloatingTabs/FloatingTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTabsApp.swift
+//  FloatingTabs
+//
+//  Created by Rob Sturcke on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
