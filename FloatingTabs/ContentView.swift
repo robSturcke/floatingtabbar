fac4ec395abbe5914003c4510a043bd995cbd1e3
Rob Sturcke <rob@rslabs.dev>
2024-01-27 12:30:36 -0500

    sets up tabs and hides default presentation

--- FloatingTabs/ContentView.swift
+++ FloatingTabs/ContentView.swift
@@ -8,17 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @StateObject private var tabModel: TabModel = .init()
+  
+  var body: some View {
+    TabView(selection: $tabModel.activeTab) {
+      Text("Home View")
+        .tag(Tab.home)
+        .background(HideTabBar())
+      
+      Text("Favorites View")
+        .tag(Tab.favorites)
+      
+      Text("Notifications View")
+        .tag(Tab.notifications)
+      
+      Text("Settings View")
+        .tag(Tab.settings)
+      
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
+}
+
+class TabModel: ObservableObject {
+  @Published var activeTab: Tab = .home
+}
+
+enum Tab: String, CaseIterable {
+  case home = "house.fill"
+  case favorites = "suit.heart.fill"
+  case notifications = "bell.fill"
+  case settings = "gearshape"
+}
+
+fileprivate struct HideTabBar: NSViewRepresentable {
+  func makeNSView(context: Context) -> NSView {
+    return .init()
+  }
+  
+  func updateNSView(_ nsView: NSView, context: Context) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.06) {
+      if let tabView = nsView.superview?.superview?.superview as? NSTabView {
+        tabView.tabViewType = .noTabsNoBorder
+        tabView.tabViewBorderType = .none
+        tabView.tabPosition = .none
+      }
+    }
+  }
 }
