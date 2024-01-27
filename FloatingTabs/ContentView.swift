feee5a3d89f573adf550b6e120a93e7ca7b7a1a8
Rob Sturcke <rob@rslabs.dev>
2024-01-27 18:00:35 -0500

    floating tabs

--- FloatingTabs/ContentView.swift
+++ FloatingTabs/ContentView.swift
@@ -13,7 +13,19 @@ struct ContentView: View {
   
   var body: some View {
     TabView(selection: $tabModel.activeTab) {
-      Text("Home View")
+      NavigationStack {
+        Text("Home View")
+          .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+              Button("", systemImage: "sidebar.left") {
+                tabModel.hideTabBar.toggle()
+              }
+            }
+          }
+      }
+      Rectangle()
+        .fill(.red)
+        .padding(10)
         .tag(Tab.home)
         .background(HideTabBar())
       
@@ -26,25 +38,41 @@ struct ContentView: View {
       Text("Settings View")
         .tag(Tab.settings)
     }
-    .customOnChange(value: state) { newValue in
+    .opacity(tabModel.isTabBarAdded ? 1 : 0)
+    .background {
+      GeometryReader {
+        let rect = $0.frame(in: .global)
+        
+        Color.clear
+          .customOnChange(value: rect) { _ in
+            tabModel.updateTabPosition()
+          }
+      }
+    }
+    .customOnChange(value: state, initial: true) { newValue in
       if newValue == .key {
         tabModel.addTabBar()
       }
     }
+    .frame(minWidth: 120, minHeight: 250)
+    .padding(.bottom, tabModel.isTabBarAdded ? 0 : 1)
   }
 }
 
 extension View {
   @ViewBuilder
-  func customOnChange(value: ControlActiveState, result: @escaping (ControlActiveState) -> ()) -> some View {
+  func customOnChange<Value: Equatable>(value: Value, initial: Bool = false, result: @escaping (Value) -> ()) -> some View {
     if #available(macOS 14, *) {
       self
-        .onChange(of: value) { oldValue, newValue in
+        .onChange(of: value, initial: initial) { oldValue, newValue in
           result(newValue)
         }
     } else {
       self
         .onChange(of: value, perform: result)
+        .onAppear {
+          result(value)
+        }
     }
   }
   
@@ -67,6 +95,7 @@ extension View {
 class TabModel: ObservableObject {
   @Published var activeTab: Tab = .home
   @Published private(set) var isTabBarAdded: Bool = false
+  @Published var hideTabBar: Bool = false
   
   private let id: String = UUID().uuidString
   
@@ -83,11 +112,23 @@ class TabModel: ObservableObject {
       let windowSize = applicationWindow.frame.size
       let windowOrigin = applicationWindow.frame.origin
       
-      floatingWindow.setFrameOrigin(.init(x: windowOrigin.x - 50, y: windowOrigin.y + (windowSize.height - 150) / 2))
+      floatingWindow.setFrameOrigin(.init(x: windowOrigin.x - 50, y: windowOrigin.y + (windowSize.height - 180) / 2))
       
       applicationWindow.addChildWindow(floatingWindow, ordered: .above)
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) { [weak self] in
+        self?.isTabBarAdded = true
+      }
     } else {
       print("WINDOW NOT FOUND")
+    }
+  }
+  
+  func updateTabPosition() {
+    if let floatingWindow = NSApplication.shared.windows.first(where: { $0.title == id }), let applicationWindow = NSApplication.shared.mainWindow {
+      let windowSize = applicationWindow.frame.size
+      let windowOrigin = applicationWindow.frame.origin
+      
+      floatingWindow.setFrameOrigin(.init(x: windowOrigin.x - 50, y: windowOrigin.y + (windowSize.height - 180) / 2))
     }
   }
 }
@@ -101,17 +142,42 @@ enum Tab: String, CaseIterable {
 
 fileprivate struct FloatingTabBarView: View {
   @EnvironmentObject private var tabModel: TabModel
+  @Environment(\.colorScheme) private var colorScheme
+  @Namespace private var animation
+  private let animationID: UUID = .init()
   
   var body: some View {
     VStack(spacing: 0) {
-      
+      ForEach(Tab.allCases, id: \.rawValue) { tab in
+        Button {
+          tabModel.activeTab = tab
+        } label: {
+          Image(systemName: tab.rawValue)
+            .font(.title3)
+            .foregroundStyle(tabModel.activeTab == tab ? (colorScheme == .dark ? .black : .white) : .primary)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background {
+              if tabModel.activeTab == tab {
+                Circle()
+                  .fill(.primary)
+                  .matchedGeometryEffect(id: animationID, in: animation)
+              }
+            }
+            .contentShape(.rect)
+            .animation(.bouncy, value: tabModel.activeTab)
+        }
+        .buttonStyle(.plain)
+      }
     }
-    .frame(width: 45, height: 150)
+    .padding(5)
+    .frame(width: 45, height: 180)
     .windowBackground()
     .clipShape(.capsule)
     .frame(maxWidth: .infinity, alignment: .leading)
     .frame(width: 50)
     .contentShape(.capsule)
+    .offset(x: tabModel.hideTabBar ? 60 : 0)
+    .animation(.snappy, value: tabModel.hideTabBar)
   }
 }
 
